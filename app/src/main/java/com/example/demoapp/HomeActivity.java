7e1cee00b796d6ae1ab9471f1c9b61ccb070ee14
vinay kumar <bbdvin007@gmail.com>
2019-02-16 10:17:21 +0530

    Merge branch 'master' of /home/vinay007/AndroidStudioProjects/DemoApp with conflicts.

--- app/src/main/java/com/example/demoapp/HomeActivity.java
+++ app/src/main/java/com/example/demoapp/HomeActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.demoapp.data.StationDummyData;
 import com.example.demoapp.models.PoliceStation;
